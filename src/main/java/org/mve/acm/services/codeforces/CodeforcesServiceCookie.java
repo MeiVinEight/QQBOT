c3e705058918df0c55a68a469562920d4078406a
MeiVinEight <MeiVinEight@outlook.com>
2024-10-16 17:17:22 +0800

    Create cookie service

---
+++ src/main/java/org/mve/acm/services/codeforces/CodeforcesServiceCookie.java
@@ -0,0 +1,29 @@
+package org.mve.acm.services.codeforces;
+
+import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.message.data.SingleMessage;
+import org.mve.acm.services.Service;
+
+import java.util.LinkedList;
+
+public class CodeforcesServiceCookie extends Service<GroupMessageEvent>
+{
+	public CodeforcesServiceCookie(String name)
+	{
+		super(name);
+	}
+
+	@Override
+	public void service(GroupMessageEvent event, LinkedList<SingleMessage> contentList)
+	{
+		String jsessionid = "";
+		if (!contentList.isEmpty()) jsessionid = contentList.getFirst().toString();
+		if (jsessionid.length() != 32)
+		{
+			event.getSubject().sendMessage("Wrong JSESSIONID");
+			return;
+		}
+
+		// TODO Database Access
+	}
+}
