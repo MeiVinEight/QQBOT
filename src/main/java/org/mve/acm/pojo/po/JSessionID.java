d4c1485cfbde96f9d8f46ab975bff3bef9e1c872
MeiVinEight <MeiVinEight@outlook.com>
2024-10-17 16:20:09 +0800

    Change JSessionID struct

--- src/main/java/org/mve/acm/pojo/po/JSessionID.java
+++ src/main/java/org/mve/acm/pojo/po/JSessionID.java
@@ -1,7 +1,15 @@
 package org.mve.acm.pojo.po;
 
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Table(name = "JSESSIONID")
 public class JSessionID
 {
-	public long ID;
+	@Column(name = "GID")
+	public Long GID;
+	@Column(name = "UID")
+	public long UID;
+	@Column(name = "JSESSIONID")
 	public String JSESSIONID;
 }
